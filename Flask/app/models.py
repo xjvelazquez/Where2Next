02d54a102a54a6c750ff00af053ddedd94ff5da2
Jaime Velazquez <jaimevelazquez22@yahoo.com>
2021-03-05 17:47:31 -0800

    Further dev

--- Flask/app/models.py
+++ Flask/app/models.py
@@ -1,10 +1,23 @@
 from app import db
+from datetime import datetime
 
+# Template for the User table in the database
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+# Template for the Post table in the database
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
