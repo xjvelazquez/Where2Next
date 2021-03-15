c7ac8d91c0e3aceeb9296c718ca79543eaa5afa8
Jaime Velazquez <jaimevelazquez22@yahoo.com>
2021-03-14 20:00:34 -0700

    Subtemplates for profiles implemented

--- Flask/app/models.py
+++ Flask/app/models.py
@@ -3,6 +3,7 @@ from app import login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 # Template for the User table in the database
 class User(UserMixin, db.Model):
@@ -21,6 +22,10 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
     
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
 
 # Template for the Post table in the database
 class Post(db.Model):
